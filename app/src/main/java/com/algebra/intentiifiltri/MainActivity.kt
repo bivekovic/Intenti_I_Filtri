b854e89c3ab9a36e251ca42527cf64da9505128c
office <bla@bla.com>
2021-06-15 20:59:57 +0200

    Commit 3

--- app/src/main/java/com/algebra/intentiifiltri/MainActivity.kt
+++ app/src/main/java/com/algebra/intentiifiltri/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.algebra.intentiifiltri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity( ) {
+
+    private lateinit var etValue: EditText
+    private lateinit var bStartSecond: Button
+    private lateinit var bImplicit: Button
+    private lateinit var bStartForResult: Button
+    private lateinit var bPickContact: Button
+
+    override fun onCreate( savedInstanceState: Bundle? ) {
         super.onCreate( savedInstanceState )
         setContentView( R.layout.activity_main )
+        initWidgets( )
+        setupListeners( )
+    }
+
+    private fun setupListeners( ) {
+        bStartSecond.setOnClickListener {
+            val intent = Intent( this, SecondActivity::class.java )
+            startActivity( intent )
+        }
+
+        bImplicit.setOnClickListener {
+            val intent = Intent( ).apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra( Intent.EXTRA_TEXT, etValue.text.toString( ) )
+            }
+
+            startActivity( intent )
+
+        }
+    }
+
+    private fun initWidgets( ) {
+        etValue = findViewById(R.id.etValue)
+        bStartSecond = findViewById(R.id.bStartSecond)
+        bImplicit = findViewById(R.id.bStartImplicit)
+        bStartForResult = findViewById(R.id.bStartAcForRes)
+        bPickContact = findViewById(R.id.bContact)
     }
 }
